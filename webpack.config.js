596bc80f58739395882eb03e027fe24e781cdc4d
Pavel Didenko <win4ester2016@yandex.ru>
2022-12-15 15:26:09 +0300

    feat: add webpack dev server to webpack.config and to dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,48 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        include: [path.resolve(__dirname, "src")],
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
+    entry: "./src/index.ts",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                include: [path.resolve(__dirname, "src")],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".js", ".css"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+            ignoreOrder: true,
+        }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js", ".css"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.html"),
-      filename: "index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-      ignoreOrder: true,
-    }),
-    new CleanWebpackPlugin(),
-  ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
 };
