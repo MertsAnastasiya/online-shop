84fc06ec3b0b8fcb5c805f633b06cb036a3eae8c
Anastasiya <nastya.mikholap@gmail.com>
2022-12-15 12:44:27 +0100

    fix: fix loading css by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,45 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.ts",
-    mode: "development",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                include: [path.resolve(__dirname, "src")],
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js", ".css"],
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "public"),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "./src/index.html"),
-            filename: "index.html",
-        }),
-        new CleanWebpackPlugin(),
+  entry: "./src/index.ts",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".css"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      ignoreOrder: true,
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
