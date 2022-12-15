ee6cbb6463ad8846c5e3dfdc7f7e5773e895336c
Pavel Didenko <win4ester2016@yandex.ru>
2022-12-15 19:19:02 +0300

    feat: add database to the project

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import './sass/header.scss';
-import './sass/styles.scss';
-console.log('Hello');
+import './sass/index-main.scss';
+import { dataBase } from './modules/database';
+console.log(dataBase);
+
