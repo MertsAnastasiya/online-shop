f529b5a06df74782965e2fc9cffdec5baf5ca4e5
Pavel Didenko <win4ester2016@yandex.ru>
2022-12-19 17:18:55 +0300

    feat: implement upload products function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,45 @@
 import './sass/styles.scss';
-// import { dataBase } from './modules/database';
 import "./modules/sliders";
+import { productsData } from './modules/data';
+import { IProduct } from './modules/interfaces/product.interface';
+
+function generateProduct(num: number, currency: string): void{
+    const products = document.querySelector(".products") as HTMLElement;
+    const productDiv: HTMLDivElement = document.createElement('div');
+    const productImg: HTMLImageElement = document.createElement('img');
+    const productSpans: HTMLDivElement = document.createElement('div');
+    const nameSpan: HTMLSpanElement = document.createElement('span');
+    const priceSpan: HTMLSpanElement = document.createElement('span');
+    const addToCart: HTMLButtonElement = document.createElement('button');
+    const productImgDiv: HTMLDivElement = document.createElement('div');
+
+
+    productDiv.className = "product";
+    productSpans.className = "product__spans";
+    productImg.setAttribute('src', productsData[num]!.images[0] as keyof IProduct);
+    productImg.setAttribute('alt', "product");
+    productImgDiv.appendChild(productImg);
+    nameSpan.className = "product__name";
+    priceSpan.className = "product__price";
+    nameSpan.innerHTML = `${productsData[num]?.title}`;
+    priceSpan.innerHTML = `${currency}${productsData[num]?.price}`;
+    addToCart.className = "button button_small add-cart";
+    addToCart.innerHTML = "Add to cart";
+
+    productSpans.appendChild(nameSpan);
+    productSpans.appendChild(priceSpan);
+
+    productImg.className = "product__img";
+    productDiv.appendChild(productImgDiv);
+    productDiv.appendChild(productSpans);
+    productDiv.appendChild(addToCart);
+    products.appendChild(productDiv);
+}
+
+function start(): void{
+    for(let i = 0; i < productsData.length - 1; i++){
+        generateProduct(i, '€');
+    };
+}
+
+start();
