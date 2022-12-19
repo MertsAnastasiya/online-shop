082b9753a6d0f722ed777e667e6dd08593219e05
Anastasiya <nastya.mikholap@gmail.com>
2022-12-19 22:58:56 +0100

    fix: change let to const

--- src/modules/filters/filters.ts
+++ src/modules/filters/filters.ts
@@ -5,30 +5,29 @@ export class Filters {
   constructor() {
 
   }
-
   public generateFilter(data: IProduct[],filterType: productProperty): void {
-    let filter = document.querySelector(`.filter_${filterType}`)!;
-    let setFilter: Set<unknown> = this.generateFilterItems(data, filterType);
+    const filter = document.querySelector(`.filter_${filterType}`)!;
+    const setFilter: Set<unknown> = this.generateFilterItems(data, filterType);
     setFilter.forEach( (item) => filter.appendChild(this.createCheckbox(item as string)));
   }
 
   private generateFilterItems(data: IProduct[], filterType: productProperty): Set<unknown>  {
-    let setItems = new Set<unknown>();
+    const setItems = new Set<unknown>();
     data.forEach( (item) => setItems.add(item[filterType]));
     return setItems;
   }
 
   private createCheckbox(value: string): HTMLElement {
-    let filterItem: HTMLElement = document.createElement('div');
+    const filterItem: HTMLElement = document.createElement('div');
     filterItem.classList.add('filter__item');
 
-    let checkbox: HTMLInputElement = document.createElement('input');
+    const checkbox: HTMLInputElement = document.createElement('input');
     checkbox.type = 'checkbox';
     checkbox.name = value;
     checkbox.value = value;
     checkbox.id = value;
     checkbox.classList.add('checkbox');
-    let label = document.createElement('label');
+    const label = document.createElement('label');
     label.classList.add('label');
     label.htmlFor = checkbox.id;
     label.textContent = value;
