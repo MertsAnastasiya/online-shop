c681be55ef0f936a660a12cc0d0359980623f631
Pavel Didenko <win4ester2016@yandex.ru>
2022-12-20 22:29:01 +0300

    fix: spaces in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,61 +6,61 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        use: "ts-loader",
-        include: [path.resolve(__dirname, "src")],
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".ts", ".js", ".css"],
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/index.html"),
-      filename: "index.html",
-    }),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./src/modules/product/product.html"),
-      filename: "product.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-      ignoreOrder: true,
-    }),
-    new CleanWebpackPlugin(),
-    new CopyPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "./src/assets"),
-          to: path.resolve(__dirname, "./public/assets"),
-        },
-      ],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+    entry: "./src/index.ts",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                include: [path.resolve(__dirname, "src")],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
     },
-    compress: true,
-    port: 9000,
-    hot: false,
-  },
+    resolve: {
+        extensions: [".ts", ".js", ".css"],
+    },
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "public"),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html"),
+            filename: "index.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "./src/modules/product/product.html"),
+            filename: "product.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "styles.css",
+            ignoreOrder: true,
+        }),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "./src/assets"),
+                    to: path.resolve(__dirname, "./public/assets"),
+                },
+            ],
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        compress: true,
+        port: 9000,
+        hot: false,
+    },
 };
