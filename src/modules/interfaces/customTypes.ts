f340b7d2ec0dd2a7763a6bf79b96c5b1e10115bf
Anastasiya <nastya.mikholap@gmail.com>
2022-12-23 01:33:53 +0100

    feat: create types for callbacks

--- src/modules/interfaces/customTypes.ts
+++ src/modules/interfaces/customTypes.ts
@@ -2,3 +2,6 @@ import { IProduct } from './product.interface';
 
 export type ProductProperty = keyof IProduct;
 export type FilterType = 'category' | 'brand';
+
+export type CallbackGlobalFilters = (filters: Map<FilterType, Set<string>>) => IProduct[];
+export type CallbackCheckboxFilters = (clientsCheckboxes: Map<FilterType, Set<string>>) => void;
