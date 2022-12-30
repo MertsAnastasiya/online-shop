4be6edc4f9f727b6eb1342bc85c159264dd56cda
Anastasiya <nastya.mikholap@gmail.com>
2022-12-30 23:28:55 +0100

    refactor: add searchParam as property

--- src/modules/searchParams.ts
+++ src/modules/searchParams.ts
@@ -1,39 +1,37 @@
 export class SearchParams {
     private static readonly unused: string = '';
+    private searchParams: URLSearchParams = new URLSearchParams(window.location.search);
 
     public updateSearchParamByCheckbox(param: string, value: string, isAdd: boolean): void {
-        const searchParams: URLSearchParams = new URLSearchParams(window.location.search);
         if (isAdd) {
-            searchParams.append(param, value);
+            this.searchParams.append(param, value);
         } else {
-            const currentParamValueArray: string[] = searchParams.getAll(param);
+            const currentParamValueArray: string[] = this.searchParams.getAll(param);
             const newCurrentValue: string[] = currentParamValueArray.filter((item) => item !== value);
             newCurrentValue.length !== 0
-                ? searchParams.set(param, newCurrentValue.join('|'))
-                : searchParams.delete(param);
+                ? this.searchParams.set(param, newCurrentValue.join('|'))
+                : this.searchParams.delete(param);
         }
-        this.updateUrl(searchParams);
+        this.updateUrl();
     }
 
     public updateSearchParamBySlider(param: string, min: string, max: string): void {
-        const searchParams: URLSearchParams = new URLSearchParams(window.location.search);
-        searchParams.set(param, `${min}/${max}`);
-        this.updateUrl(searchParams);
+        this.searchParams.set(param, `${min}/${max}`);
+        this.updateUrl();
     }
 
     public updateSearchParamBySearch(param: string, value: string): void {
-        const searchParams: URLSearchParams = new URLSearchParams(window.location.search);
-        value !== '' ? searchParams.set(param, value) : searchParams.delete(param);
-        this.updateUrl(searchParams);
+        value !== '' ? this.searchParams.set(param, value) : this.searchParams.delete(param);
+        this.updateUrl();
     }
 
-    private updateUrl(searchParams: URLSearchParams): void {
-        if (searchParams.toString() !== '') {
-            searchParams.sort();
+    private updateUrl(): void {
+        if (this.searchParams.toString() !== '') {
+            this.searchParams.sort();
             window.history.pushState(
-                Object.fromEntries(searchParams.entries()),
+                Object.fromEntries(this.searchParams.entries()),
                 SearchParams.unused,
-                `${window.location.pathname}?${searchParams.toString()}`
+                `${window.location.pathname}?${this.searchParams.toString()}`
             );
         } else {
             window.history.pushState({}, SearchParams.unused, window.location.pathname);
