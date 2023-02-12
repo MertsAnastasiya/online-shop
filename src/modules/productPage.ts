17f652d264534fb85b7a93ee2cc54215ac2be0b5
Anastasiya <nastya.mikholap@gmail.com>
2023-02-12 22:54:18 +0100

    refactor: refactor class ProductPage

--- src/modules/productPage.ts
+++ src/modules/productPage.ts
@@ -1,23 +1,32 @@
 'use strict';
 import { productsData } from './data';
-import { OnButtonCartClick, OnButtonClick, PageButtons } from './interfaces/customTypes';
+import {
+    OnButtonCartClick,
+    OnButtonClick,
+    PageButtons,
+} from './interfaces/customTypes';
 import { IProduct } from './interfaces/product.interface';
 
-const IMAGE_COUNT = 3;
-
 export class ProductPage {
-    private parent: Element;
-    private productDataLayout: string;
-    private product: IProduct;
-    private onButtonClick: OnButtonClick;
-    private onProductSelected: OnButtonCartClick;
+    private readonly parent: Element;
+    private readonly productDataLayout: string;
+    private readonly product: IProduct;
+    private readonly onButtonClick: OnButtonClick;
+    private readonly onProductSelected: OnButtonCartClick;
+    private readonly imageCount = 3;
 
-    constructor(parent: Element, id: number, onProductSelected: OnButtonCartClick, onButtonClick: OnButtonClick) {
+    constructor(
+        parent: Element,
+        id: number,
+        onProductSelected: OnButtonCartClick,
+        onButtonClick: OnButtonClick
+    ) {
+        this.parent = parent;
         this.onButtonClick = onButtonClick;
         this.onProductSelected = onProductSelected;
+
         this.product = productsData.filter((data) => data.id === id)[0]!;
 
-        this.parent = parent;
         this.productDataLayout = `
             <div class="route"></div>
             <div class="product-info">
@@ -48,15 +57,17 @@ export class ProductPage {
         <div class="disabled-area hidden"></div>`;
     }
 
-    public drawProductPage(selectedArray: number[]) {
+    public drawProductPage(selectedArray: number[]): void {
         this.parent.innerHTML = this.productDataLayout;
         const route: Element = document.querySelector('.route')!;
         const productName: Element = document.querySelector('.name')!;
         const brand: Element = document.querySelector('.brand')!;
         const description: Element = document.querySelector('.description')!;
         const buttonAddToCart: Element = document.querySelector('.add-cart')!;
-        let isAddedToCart = Boolean(selectedArray.filter((item) => item === this.product.id)[0]);
-        if(isAddedToCart) {
+        let isAddedToCart: boolean = Boolean(
+            selectedArray.filter((item) => item === this.product.id)[0]
+        );
+        if (isAddedToCart) {
             buttonAddToCart.innerHTML = 'Remove from cart';
             buttonAddToCart.classList.add('remove-from-cart');
             isAddedToCart = false;
@@ -65,38 +76,46 @@ export class ProductPage {
             buttonAddToCart.classList.add('add-to-cart');
             isAddedToCart = true;
         }
-        const buyButton: Element = document.querySelector('.button_buy')! as HTMLButtonElement;
+        const buyButton: HTMLButtonElement =
+            document.querySelector<HTMLButtonElement>('.button_buy')!;
         const price: Element = document.querySelector('.price')!;
         const rating: Element = document.querySelector('.rating')!;
-        const mainImg = document.querySelector('.main-image')! as HTMLImageElement;
-        const addImgWrapper: Element = document.querySelector('.additional-images__wrapper')!;
+        const mainImg: HTMLImageElement =
+            document.querySelector<HTMLImageElement>('.main-image')!;
+        const addImgWrapper: Element = document.querySelector(
+            '.additional-images__wrapper'
+        )!;
 
         const routeText: string = `Store / ${this.product.category} / ${this.product.brand} / ${this.product.title}`;
-        route.innerHTML = routeText;
+        route.textContent = routeText;
         productName.innerHTML = this.product.title;
-        brand!.innerHTML = this.product.brand;
-        description!.innerHTML = this.product.description;
+        brand!.textContent = this.product.brand;
+        description!.textContent = this.product.description;
         buttonAddToCart.setAttribute('id', this.product.id.toString());
-        price!.innerHTML = `€${this.product.price}`;
-        rating!.innerHTML = `Rating: ${this.product.rating}`;
+        price!.textContent = `€${this.product.price}`;
+        rating!.textContent = `Rating: ${this.product.rating}`;
         if (this.product.images[0]) {
             mainImg.src = this.product.images[0]!;
         }
 
-        let i = 0;
-        while (i < IMAGE_COUNT) {
+        let i: number = 0;
+        while (i < this.imageCount) {
             if (this.product.images[i]) {
-                const addImg = document.createElement('img');
-                addImg.classList.add('additional-image')
+                const addImg: HTMLImageElement = document.createElement('img');
+                addImg.classList.add('additional-image');
                 addImg.src = this.product.images[i]!;
                 addImg.addEventListener('click', () => {
-                        mainImg.src = addImg.src;
+                    mainImg.src = addImg.src;
                 });
                 addImgWrapper.appendChild(addImg);
             }
             i++;
         }
-        buttonAddToCart.addEventListener('click', (event) => this.onProductSelected(event, this.product.id));
-        buyButton.addEventListener('click', () => this.onButtonClick(PageButtons.Buy));
+        buttonAddToCart.addEventListener('click', (event) =>
+            this.onProductSelected(event, this.product.id)
+        );
+        buyButton.addEventListener('click', () =>
+            this.onButtonClick(PageButtons.Buy)
+        );
     }
 }
