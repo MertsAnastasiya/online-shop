c0010a1e113fa370977b4913e4ec68166eff3ca6
Anastasiya <nastya.mikholap@gmail.com>
2023-02-12 22:07:44 +0100

    refactor: format type files'

--- src/modules/interfaces/customTypes.ts
+++ src/modules/interfaces/customTypes.ts
@@ -6,23 +6,43 @@ export type FilterType = 'category' | 'brand';
 
 export type SliderType = 'price' | 'stock';
 
-export type CallbackOnClickCheckbox = (filterType: FilterType, value: string, isAdded: boolean) => void;
+export type CallbackOnClickCheckbox = (
+    filterType: FilterType,
+    value: string,
+    isAdded: boolean
+) => void;
 
-export type CallbackViewChanged = (sliderType: SliderType, currentSliderValue: SliderValue) => void;
+export type CallbackViewChanged = (
+    sliderType: SliderType,
+    currentSliderValue: SliderValue
+) => void;
 
-export type CallbackOnChangeFilters = (currentFilters: Map<FilterType, Set<string>>, currentSliders: Map<SliderType, SliderValue>, searchValue: string, sort: string) => void;
+export type CallbackOnChangeFilters = (
+    currentFilters: Map<FilterType, Set<string>>,
+    currentSliders: Map<SliderType, SliderValue>,
+    searchValue: string,
+    sort: string
+) => void;
 
-export type ToChangeParamsByCheckbox = (param: string, value: string, isAdd: boolean) => void;
+export type ToChangeParamsByCheckbox = (
+    param: string,
+    value: string,
+    isAdd: boolean
+) => void;
 
-export type ToChangeParamsBySlider = (param: string, min: string, max: string) => void;
+export type ToChangeParamsBySlider = (
+    param: string,
+    min: string,
+    max: string
+) => void;
 
 export type ToChangeParamsBySearch = (param: string, value: string) => void;
 
 export type ToChangeParamsBySort = (param: string) => void;
 
 export type SliderValue = {
-    'min': number;
-    'max': number;
+    min: number;
+    max: number;
 };
 
 export type OnButtonCartClick = (event: Event, productId: number) => void;
