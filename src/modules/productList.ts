e4c089f02453b6f7e527647aa052fc4429678fab
Anastasiya <nastya.mikholap@gmail.com>
2023-02-12 22:49:40 +0100

    refactor: refactor class ProductList

--- src/modules/productList.ts
+++ src/modules/productList.ts
@@ -1,13 +1,18 @@
-import { OnButtonCartClick, OnProductClick} from './interfaces/customTypes';
+import { OnButtonCartClick, OnProductClick } from './interfaces/customTypes';
 import { IProduct } from './interfaces/product.interface';
 
 export class ProductList {
-    private parent: Element;
-    private onButtonCartClick: OnButtonCartClick;
-    private onProductClick: OnProductClick;
-    private checkButtonStatus: (id: number) => boolean;
+    private readonly parent: Element;
+    private readonly onButtonCartClick: OnButtonCartClick;
+    private readonly onProductClick: OnProductClick;
+    private readonly checkButtonStatus: (id: number) => boolean;
 
-    constructor(parent: Element, onButtonCartClick: OnButtonCartClick, onProductClick: OnProductClick, checkButtonStatus: (id: number) => boolean) {
+    constructor(
+        parent: Element,
+        onButtonCartClick: OnButtonCartClick,
+        onProductClick: OnProductClick,
+        checkButtonStatus: (id: number) => boolean
+    ) {
         this.parent = parent;
         this.onButtonCartClick = onButtonCartClick;
         this.onProductClick = onProductClick;
@@ -18,46 +23,68 @@ export class ProductList {
         this.parent.innerHTML = '';
 
         productsList.forEach((product) => {
-            const productDiv: HTMLDivElement = document.createElement('div');
-            const productImg: HTMLImageElement = document.createElement('img');
-            const productSpans: HTMLDivElement = document.createElement('div');
-            const nameTitle: HTMLSpanElement = document.createElement('span');
-            const priceSpan: HTMLSpanElement = document.createElement('span');
-            const buttonAddToCart: HTMLButtonElement = document.createElement('button');
-            const productImgDiv: HTMLDivElement = document.createElement('div');
+            const productWrapper: HTMLDivElement =
+                document.createElement('div');
+            productWrapper.classList.add('product');
+            productWrapper.addEventListener('click', () =>
+                this.onProductClick(product.id)
+            );
 
-            productDiv.className = 'product';
-            productSpans.className = 'product__spans';
-            productImg.setAttribute('src', product.images[0]!);
-            productImg.setAttribute('alt', 'product');
-            productImgDiv.appendChild(productImg);
-            nameTitle.className = 'product__name';
-            priceSpan.className = 'product__price';
-            nameTitle.innerHTML = product.title;
-            priceSpan.innerHTML = product.price.toString();
-            buttonAddToCart.className = 'button button_small';
-            if (this.checkButtonStatus(product.id)) {
-                buttonAddToCart.innerHTML = 'Remove from cart';
-                buttonAddToCart.classList.add('remove-from-cart');
-            } else {
-                buttonAddToCart.innerHTML = 'Add to cart';
-                buttonAddToCart.classList.add('add-to-cart');
-            }
+            productWrapper.appendChild(this.createProductImage(product.images));
+            productWrapper.appendChild(
+                this.createProductTitle(product.title, product.price)
+            );
+            productWrapper.appendChild(this.createButtonAddToCart(product.id));
 
-            buttonAddToCart.addEventListener('click', (event) => {
-                event.stopPropagation();
-                this.onButtonCartClick(event, product.id);
-            });
-            productDiv.addEventListener('click', () => this.onProductClick(product.id));
-            productSpans.appendChild(nameTitle);
-            productSpans.appendChild(priceSpan);
-
-            productImg.className = 'product__img';
-            productDiv.appendChild(productImgDiv);
-            productDiv.appendChild(productSpans);
-            productDiv.appendChild(buttonAddToCart);
-
-            this.parent.appendChild(productDiv);
+            this.parent.appendChild(productWrapper);
         });
+    }
+
+    private createProductImage(images: string[]): Element {
+        const productImgDiv: HTMLDivElement = document.createElement('div');
+        const productImg: HTMLImageElement = document.createElement('img');
+        productImg.setAttribute('src', images[0]!);
+        productImg.setAttribute('alt', 'product');
+        productImg.classList.add('product__img');
+        productImgDiv.appendChild(productImg);
+
+        return productImgDiv;
+    }
+
+    private createProductTitle(title: string, price: number): Element {
+        const productSpans: HTMLDivElement = document.createElement('div');
+        const nameTitle: HTMLSpanElement = document.createElement('span');
+        const priceSpan: HTMLSpanElement = document.createElement('span');
+
+        productSpans.classList.add('product__spans');
+        nameTitle.classList.add('product__name');
+        priceSpan.classList.add('product__price');
+        nameTitle.textContent = title;
+        priceSpan.textContent = `€‎${price}`;
+
+        productSpans.appendChild(nameTitle);
+        productSpans.appendChild(priceSpan);
+
+        return productSpans;
+    }
+
+    private createButtonAddToCart(id: number): Element {
+        const buttonAddToCart: HTMLButtonElement =
+            document.createElement('button');
+        buttonAddToCart.classList.add('button');
+        buttonAddToCart.classList.add('button_small');
+        if (this.checkButtonStatus(id)) {
+            buttonAddToCart.innerHTML = 'Remove from cart';
+            buttonAddToCart.classList.add('remove-from-cart');
+        } else {
+            buttonAddToCart.innerHTML = 'Add to cart';
+            buttonAddToCart.classList.add('add-to-cart');
+        }
+
+        buttonAddToCart.addEventListener('click', (event) => {
+            event.stopPropagation();
+            this.onButtonCartClick(event, id);
+        });
+        return buttonAddToCart;
     }
 }
